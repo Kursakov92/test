26cb77275f6e493759d24daa6c707954e9af1686
Kursakov Alexnder <sashavalezhnik@yandex.ru>
2023-08-30 15:14:44 +0300

    first commit

---
+++ tests/capitalize.test.js
@@ -0,0 +1,7 @@
+import { strict as assert } from 'node:assert';
+import capitalize from '../src/capitalize.js';
+
+assert(capitalize('') === '');
+assert(capitalize('hello') === 'Hello');
+
+console.log('Все тесты пройдены!');
